ce5f7955e53140138f3dd956a5f4517afa15f058
Max <inside162544@gmail.com>
2024-04-05 11:54:04 +0700

    update a little  bit in navbar

--- app.py
+++ app.py
@@ -1,46 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session
 
 app = Flask(__name__)
-
-# Function to generate navigation bar HTML
-def generate_navbar():
-    return '''
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-      <a class="navbar-brand" href="#">My Website</a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav mr-auto">
-          <li class="nav-item active">
-            <a class="nav-link" href="/">Home</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="/about">About</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="/contact">Contact</a>
-          </li>
-        </ul>
-      </div>
-    </nav>
-    '''
+app.secret_key = 'your_secret_key_here'  # Set a secret key for session management
 
 @app.route('/')
 def index():
-    navbar = generate_navbar()
-    return render_template('index.html', navbar=navbar)
-
-@app.route('/about')
-def about():
-    navbar = generate_navbar()
-    return render_template('about.html', navbar=navbar)
-
-@app.route('/contact')
-def contact():
-    navbar = generate_navbar()
-    return render_template('contact.html', navbar=navbar)
+    if 'id' not in session:
+        return render_template('index.html', logged_in=False)
+    else:
+        return render_template('index.html', logged_in=True, username=session['username'])
 
 if __name__ == '__main__':
     app.run(debug=True)
