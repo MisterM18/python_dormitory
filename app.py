403da6f03499c414df7ebf5ba520177a718ba665
Max <inside162544@gmail.com>
2024-04-05 09:56:18 +0700

    create new python

--- app.py
+++ app.py
@@ -1,31 +1,46 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template
 
-app = Flask(__name__, template_folder="template")
-app.static_folder = "static"
-# รายชื่อผู้ใช้งาน (ตัวอย่าง)
-users = {"user1": "password1", "user2": "password2"}
+app = Flask(__name__)
 
+# Function to generate navigation bar HTML
+def generate_navbar():
+    return '''
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <a class="navbar-brand" href="#">My Website</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
 
-@app.route("/")
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="/">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/about">About</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/contact">Contact</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+    '''
+
+@app.route('/')
 def index():
-    return render_template("index.html")
+    navbar = generate_navbar()
+    return render_template('index.html', navbar=navbar)
 
+@app.route('/about')
+def about():
+    navbar = generate_navbar()
+    return render_template('about.html', navbar=navbar)
 
-@app.route("/contact")
+@app.route('/contact')
 def contact():
-    return render_template("contact.html")
+    navbar = generate_navbar()
+    return render_template('contact.html', navbar=navbar)
 
-
-@app.route("/do")
-def do():
-    return render_template("do.html")
-
-
-@app.route("/portfolio")
-def portfolio():
-    return render_template("portfolio.html")
-
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0")
-    # app.run(debug=True)
+if __name__ == '__main__':
+    app.run(debug=True)
