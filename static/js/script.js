d5213bf2c300dbabae711a277c54349e98f449af
Max <inside162544@gmail.com>
2024-04-07 12:45:28 +0700

    update card to responsive and a little bit navbar
    update card to responsive and a little bit navbar

---
+++ static/js/script.js
@@ -0,0 +1,14 @@
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("goToTopBtn").style.display = "block";
+  } else {
+    document.getElementById("goToTopBtn").style.display = "none";
+  }
+}
+
+function goToTop() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
