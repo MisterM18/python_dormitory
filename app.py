d1cec9f3f03fa1bf52fa7d86c5c61d7adfc33233
Max <inside162544@gmail.com>
2024-04-08 08:34:09 +0700

    update page index to looking detail in dormotory
    update page index to looking detail in dormotory

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, session
+from flask import Flask, render_template, session, redirect, url_for
 
 app = Flask(__name__)
 app.secret_key = 'your_secret_key_here'  # Set a secret key for session management
@@ -9,6 +9,12 @@ def index():
         return render_template('index.html', logged_in=False)
     else:
         return render_template('index.html', logged_in=True, username=session['username'])
+
+# Define a new route for the page you want to redirect to
+@app.route('/other_page')
+def other_page():
+    # You can render a template for this page if needed
+    return render_template('dormitory.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
